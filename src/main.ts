35173051e6edd7c67b81be89f709ef04c0eb7acf
AminRezaei-68 <rezaei.amin@gmail.com>
2025-02-01 17:01:50 +0330

    upgrade project

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    // await app.listen(process.env.PORT ?? 3000);
 
-  app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}/api`);
+    app.use(cookieParser());
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Application is running on: http://localhost:${port}/api`);
 }
 bootstrap();
